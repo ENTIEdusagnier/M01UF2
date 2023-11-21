0f74627b46119ff6058cffec2248f012ebad0691
Eduard Sagnier <eduard.sagnier@enti.cat>
2023-11-21 13:55:03 +0100

    Cliente Envia IP i Server verifica i coge IP para enviar

--- EFPT/server.sh
+++ EFPT/server.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-CLIENT="localhost"
 PORT=3333
 
 echo "Server de EFPT"
 echo "(0) Listen"
 DATA=`nc -l -p $PORT -w 0`
 echo $DATA
-PATRON_IP="([0-9]{1,3}\.){3}[0-9]{1,3}"
-
+PATRON_IP='([0-9]*\.){3}[0-9]*'
+CLIENT=`echo "$DATA" | grep -Eo "$PATRON_IP"`
+echo "$CLIENT" 
 
 echo "(3) Test & Send"
-if ! echo "$DATA" | grep -Eq "EFPT 1.0 $patron_ip";then
+if [ "$DATA" != "EFPT 1.0 "$CLIENT"" ];then
 	echo "Entrada incorrecta"
 	sleep 1
-	echo "KO Heather" | nc $CLIENT $PORT
+	echo "KO Heather" | nc "$CLIENT" $PORT
 	exit 1
 fi
 
 echo "Entrada correcta"
 sleep 2
-echo "OK Heather" | nc $CLIENT $PORT
+echo "OK Heather" | nc "$CLIENT" $PORT
 
 
 echo "(4) Listen"
@@ -32,13 +32,13 @@ echo "(7) Test & Send"
 if [ "$DATA1" != "BOOOM" ];then
 	echo "Entrada incorrecta"
 	sleep 1
-    echo "KO Heather" | nc $CLIENT $PORT
+    echo "KO Heather" | nc "$CLIENT" $PORT
     exit 2
 fi
 
 echo "Entrada correcta"
 sleep 2
-echo "OK Heather" | nc $CLIENT $PORT
+echo "OK Heather" | nc "$CLIENT" $PORT
 
 
 echo "(8) Listen"
