1cc64f82db1e32a7b144c5704ae92348ef54ba02
Eduard Sagnier <eduard.sagnier@enti.cat>
2023-11-30 13:44:38 +0100

    Mejoras varias y comunicacion

--- EFPT/client.sh
+++ EFPT/client.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-SERVER="localhost"
+SERVER="10.65.0.57"
 PORT=3333
 MYIP=`ip address | grep -Eo 'inet ([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 TIMEOUT=1
 
 echo "Cliente de EFPT"
 echo "(1) Send"
-echo "EFPT 1.0 $MYIP" | nc $SERVER $PORT
+echo "EFTP 1.0 $MYIP" | nc $SERVER $PORT
 
 
 echo "(2) Listen"
 DATA=`nc -l -p $PORT -w $TIMEOUT`
 
 echo "(5) Test & Send"
-if [ "$DATA" != "OK_HEATHER" ];then
-	echo "KO_HEATHER"
+if [ "$DATA" != "OK_HEADER" ];then
+	echo "KO_HEADER" | nc $SERVER $PORT
 	exit 1
 fi
 sleep 2
@@ -24,26 +24,26 @@ echo "BOOOM" | nc $SERVER $PORT
 
 echo "(6) Listen"
 DATA1=`nc -l -p $PORT -w $TIMEOUT`
-echo $DATA1
+#echo $DATA1
 
 echo "(9) Test HandShake"
-if [ "$DATA1" != "OK_HEATHER" ];then
-	echo "KO_HEATHER"
+if [ "$DATA1" != "OK_HEADER" ];then
+	#echo "KO_HEATHER"
 	sleep 2
-	echo "KO_HEATHER" | nc $SERVER $PORT
+	echo "KO_HEADER" | nc $SERVER $PORT
 	exit 2
 fi
 
 
 sleep 2
 echo "(10) Send file"
-FILENAME="fary1.txt"
+FILENAME="fary2.txt"
 HASH_FILENAME=`echo "$FILENAME" | md5sum | awk '{print $1}'`
 echo "FILE_NAME $FILENAME $HASH_FILENAME" | nc $SERVER $PORT
 
 echo "(11) Listen"
 FILENAME_CHECK=`nc -l -p $PORT -w $TIMEOUT`
-echo $FILENAME_CHECK
+#echo $FILENAME_CHECK
 
 echo "(11.5) Check"
 if [ "$FILENAME_CHECK" != "OK_FILE_NAME" ];then
@@ -52,31 +52,33 @@ if [ "$FILENAME_CHECK" != "OK_FILE_NAME" ];then
 fi
 
 echo "(14) Send file"
-CONV_FILE=`img2txt /home/enti/M01UF2/EFPT/img/fary1.jpg > /home/enti/M01UF2/EFPT/img/img.txt`
-SEND_FILE=`cat /home/enti/M01UF2/EFPT/img/img.txt | nc $SERVER $PORT`
-echo "File Sent"
+CONV_FILE=`img2txt /home/enti/M01UF2/EFPT/img/fary2.jpg > /home/enti/M01UF2/EFPT/img/img2.txt`
+SEND_FILE=`cat /home/enti/M01UF2/EFPT/img/img2.txt | nc $SERVER $PORT`
+#echo "File Sent"
 
 echo "(15) Listen"
 FILE_OK=`nc -l -p $PORT -w $TIMEOUT`
-echo "$FILE_OK"
+#echo "$FILE_OK"
 
 if [ "$FILE_OK" != "OK_DATA" ]; then
-	echo "KO DATA"
+	echo "KO DATA" | nc $SERVER $PORT
 	exit 4
 fi
 
 echo "(18) Send Hash"
-CREATE_HASH=`md5sum /home/enti/M01UF2/EFPT/img/img.txt | awk '{print $1}'`
-echo $CREATE_HASH
+CREATE_HASH=`md5sum /home/enti/M01UF2/EFPT/img/img2.txt | awk '{print $1}'`
+#echo $CREATE_HASH
 sleep 2
-SEND_HASH=`echo "$CREATE_HASH" | nc "$SERVER" "$PORT"`
+SEND_HASH=`echo "FILE_MD5 $CREATE_HASH" | nc "$SERVER" "$PORT"`
 
 
-echo "(19) Listen & Check"
+echo "(19) Listen"
 FILE_OK=`nc -l -p $PORT -w $TIMEOUT`
-echo "$FILE_OK"
+#echo "$FILE_OK"
 
-if [ "$FILE_OK" != "OK_DATA" ];then
+
+echo "(21) Test"
+if [ "$FILE_OK" != "OK_FILE_MD5" ];then
 	#To resend file until is OK
 	#while [ "$FILE_OK" != "OK_DATA" ]
 	#do
@@ -93,3 +95,4 @@ if [ "$FILE_OK" != "OK_DATA" ];then
 fi
 
 echo "Succesful"
+exit 0
--- EFPT/server.sh
+++ EFPT/server.sh
@@ -6,22 +6,26 @@ TIMEOUT=1
 echo "Server de EFPT"
 echo "(0) Listen"
 DATA=`nc -l -p $PORT -w $TIMEOUT`
-echo $DATA
-PATRON_IP='([0-9]*\.){3}[0-9]*'
-CLIENT=`echo "$DATA" | grep -Eo "$PATRON_IP"`
-echo "$CLIENT" 
+
+PREFIX=`echo "$DATA" | awk '{print $1}'`
+VERSION=`echo "$DATA" | awk '{print $2}'`
+echo "Prfix $CLIENT: $PREFIX"
+echo "Version $CLIENT: $VERSION "
 
 echo "(3) Test & Send"
-if [ "$DATA" != "EFPT 1.0 $CLIENT" ];then
-	echo "KO_HEATHER"
+if [ "$PREFIX $VERSION" != "EFTP 1.0" ];then
+	echo "To $CLIENT KO_HEADER"
 	sleep 3
-	echo "KO_HEATHER" | nc "$CLIENT" $PORT
+	echo "KO_HEADER" | nc "$CLIENT" $PORT
 	exit 1
 fi
 
-echo "OK_HEATHER"
+CLIENT=`echo "$DATA" | awk '{print $3}'`
+echo "IP Client: $CLIENT" 
+
+echo "To $CLIENT OK_HEADER"
 sleep 2
-echo "OK_HEATHER" | nc "$CLIENT" $PORT
+echo "OK_HEADER" | nc "$CLIENT" $PORT
 
 
 echo "(4) Listen"
@@ -31,25 +35,25 @@ echo $DATA1
 
 echo "(7) Test & Send"
 if [ "$DATA1" != "BOOOM" ];then
-	echo "KO_HEATHER"
+	echo "To $CLIENT KO_HEADER"
 	sleep 1
-    echo "KO_HEATHER" | nc "$CLIENT" $PORT
+    echo "KO_HEADER" | nc "$CLIENT" $PORT
     exit 2
 fi
 
-echo "OK_HEATHER"
+echo "To $CLIENT OK_HEADER"
 sleep 2
-echo "OK_HEATHER" | nc "$CLIENT" $PORT
+echo "OK_HEADER" | nc "$CLIENT" $PORT
 
 
 echo "(8) Listen"
 FILENAME=`nc -l -p $PORT -w $TIMEOUT`
-echo $FILENAME
+
 
 echo "(12) Test & Store & Send"
 FILE_NAME=`echo "$FILENAME" | awk '{print $1}'`
 if [ "$FILE_NAME" != "FILE_NAME" ]; then
-	echo "KO_FILE_NAME PREFIX"
+	echo "To $CLIENT KO_FILE_NAME PREFIX"
     sleep 2
     echo "KO_FILE_NAME" | nc "$CLIENT" $PORT
     exit 3
@@ -57,22 +61,22 @@ fi
 
 NAME_FILE=`echo "$FILENAME" | awk '{print $2}'`
 HASH_FILENAME=`echo "$FILENAME" | awk '{print $3}'`
-echo "$NAME_FILE"
-echo "$HASH_FILENAME"
+echo "Name of the File recived: $NAME_FILE"
+echo "Hash of the name recived: $HASH_FILENAME"
 CHECK_FILE_HASH=`echo "$NAME_FILE" | md5sum | awk '{print $1}'`
 
 if [ "$HASH_FILENAME" != "$CHECK_FILE_HASH" ];then
-	echo "KO_FILE_NAME MD5"
+	echo "To $CLIENT KO_FILE_NAME MD5"
     sleep 2
     echo "KO_FILE_NAME" | nc "$CLIENT" $PORT
     exit 3
 fi
 
-echo "OK_FILE_NAME"
+echo "To $CLIENT OK_FILE_NAME"
 sleep 2
 echo "OK_FILE_NAME" | nc "$CLIENT" $PORT
 
-echo "(13) Listen File & Listen Hash"
+echo "(13) Listen File"
 nc -l -p $PORT -w $TIMEOUT > /home/enti/M01UF2/EFPT/inbox/"$CLIENT"_"$NAME_FILE"
 
 
@@ -81,20 +85,31 @@ echo "(16) Store & Send"
 FILE=`cat /home/enti/M01UF2/EFPT/inbox/"$CLIENT"_"$NAME_FILE"`
 
 if [ -z "$FILE" ];then
-	echo "EMPTY DATA"
+	echo "To $CLIENT (EMPTY DATA) KO_DATA"
 	sleep 1
 	echo "KO_DATA" | nc $CLIENT $PORT
 	exit 4
 fi
-
+echo "To $CLIENT OK_DATA"
 echo "OK_DATA" | nc $CLIENT $PORT
 
 echo "(17) Listen Hash"
 HASH=`nc -l -p $PORT -w $TIMEOUT`
-echo $HASH
+echo "Hash from $CLIENT 's file $HASH"
 
 
-echo "(18) Test & Send (Hash)"
+echo "(20) Test & Send (Hash)"
+
+PREFIX=`echo "$HASH" | awk '{print $1}'`
+
+if [ $PREFIX != "FILE_MD5" ];then
+	echo "To $CLIENT ERROR Prefix FILE_MD5"
+	wait 1
+	echo "KO_DATA" | nc $CLIENT $PORT
+	exit 6
+fi
+
+HASH_FILE=`echo "$HASH" | awk '{print $2}'`
 CREATE_HASH=`md5sum /home/enti/M01UF2/EFPT/inbox/"$CLIENT"_"$NAME_FILE"| awk '{print $1}'`
 
 #Request file util its correct
@@ -107,12 +122,13 @@ CREATE_HASH=`md5sum /home/enti/M01UF2/EFPT/inbox/"$CLIENT"_"$NAME_FILE"| awk '{p
 	#CREATE_HASH=`md5sum /home/enti/M01UF2/EFPT/inbox/"$CLIENT"_"$NAME_FILE" | awk '{print $1}'`
 #done
 
-if [ "$HASH" != "$CREATE_HASH"  ];then
-	echo "MD5 it's not equal"
-	echo "KO_DATA" | nc $CLIENT $PORT
-	exit 5
+if [ "$HASH_FILE" != "$CREATE_HASH"  ];then
+	echo "To $CLIENT MD5 it's not equal"
+	wait 1
+	echo "KO_FILE_MD5" | nc $CLIENT $PORT
+	exit 6
 fi
 
-echo "OK_DATA" | nc $CLIENT $PORT
-echo "OK_DATA FILE RECIVED PERFECTLY"
+echo "OK_FILE_MD5" | nc $CLIENT $PORT
+echo "To $CLIENT OK_FILE_MD5 RECIVED PERFECTLY"
 exit 0
