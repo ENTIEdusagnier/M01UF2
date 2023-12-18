a199cc191b500de62ae63d836b0124bfdec75bc0
ENTIEdusagnier <151615579+ENTIEdusagnier@users.noreply.github.com>
2023-12-18 09:01:39 +0100

    Update firewall.sh

--- firewall/firewall.sh
+++ firewall/firewall.sh
@@ -6,8 +6,8 @@ iptables -P DROP
 
 iptables -A INPUT -s 10.65.0.15 -p tcp --dport 2222 -j ACCEPT
 
-iptables -A OUTPUT -d 10.65.0.15 -p tcp --sport 2222 -j ACCEPT
+iptables -A OUTPUT -d 10.65.0.15 -p tcp --dport 2222 -j ACCEPT
 
 iptables -A INPUT -p tcp --dport 3333 -j ACCEPT
 
-iptables -A OUTPUT -p tcp --sport 3333 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 3333 -j ACCEPT
