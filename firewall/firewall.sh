f5b4b047de5fa9e4627db9326876fd915a822516
Eduard Sagnier <eduard.sagnier@enti.cat>
2023-12-19 09:03:14 +0100

    Update firewall rules.

--- firewall/firewall.sh
+++ firewall/firewall.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-iptables -F
+./firewall_reset.sh
 
-iptables -P DROP
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
 
 iptables -A INPUT -s 10.65.0.15 -p tcp --dport 2222 -j ACCEPT
 
-iptables -A OUTPUT -d 10.65.0.15 -p tcp --dport 2222 -j ACCEPT
+iptables -A OUTPUT -d 10.65.0.15 -p tcp --sport 2222 -j ACCEPT
 
 iptables -A INPUT -p tcp --dport 3333 -j ACCEPT
 
-iptables -A OUTPUT -p tcp --dport 3333 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 3333 -j ACCEPT
